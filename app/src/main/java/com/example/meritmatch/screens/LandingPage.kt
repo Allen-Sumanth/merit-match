d4929820bb6ea970ecf41f6478fee1baa8e5640e
Star <allensumanth.07@gmail.com>
2024-08-02 01:41:10 +0530

    cleanup

--- app/src/main/java/com/example/meritmatch/screens/LandingPage.kt
+++ app/src/main/java/com/example/meritmatch/screens/LandingPage.kt
@@ -24,11 +24,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.example.meritmatch.R
 import com.example.meritmatch.navigation.Screens
 import com.example.meritmatch.ui.theme.HomeScreenButtonColor
